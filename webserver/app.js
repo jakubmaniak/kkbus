897027b703403d6d65e68691416bdaa88b71cf2d
Jakub Maniak <punisher4ce@gmail.com>
2021-03-06 16:23:11 +0100

    Dodanie aplikacji serwerowej

---
+++ webserver/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mysql = require('mysql');
+const fs = require('fs');
+
+const config = JSON.parse(fs.readFileSync('config.json'));
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(config.server.port, config.server.host, () => {
+    console.log(`Listening on ${config.server.host}:${config.server.port}...`);
+});
