feb15bde274a5d1fe33dd4082a592cc66460a919
Jakub Maniak <punisher4ce@gmail.com>
2021-03-06 20:10:56 +0100

    Implementacja biblioteki React

---
+++ webpage/src/App.js
@@ -0,0 +1,12 @@
+import background from './static/background.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={background} />
+    </div>
+  );
+}
+
+export default App;
