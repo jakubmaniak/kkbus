319e6fc41f5a44d6b2140f9f2f6f0124abdcfbc6
ania-r <80163377+ania-r@users.noreply.github.com>
2021-03-07 13:05:41 +0100

    Modyfikacja strony kontaktowej

--- webpage/src/components/ContactPage.js
+++ webpage/src/components/ContactPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Header from './Header';
+import '../styles/ContactPage.css';
 
 function ContactPage() {
     return (
         <div className="contact-page">
             <Header />
+            <div className="main">
+                <div className="contact">
+                    <h2>Kontakt</h2>
+                    <p>ul.Władysława Łokietka 21/3</p>
+                    <p>30-010 Kraków</p>
+                    <div>
+                        <span>Telefon: </span>
+                        <span>12 01 02 300</span>
+                    </div>
+                    <div>
+                        <span>Adres e-mail: </span>
+                        <span>kontakt@kkbus.pl</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
