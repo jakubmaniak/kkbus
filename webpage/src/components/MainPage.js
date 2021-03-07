152551f4f7581ac5b1ec17a268cd4d2a6263cd9e
ania-r <80163377+ania-r@users.noreply.github.com>
2021-03-07 17:05:50 +0100

    Dodanie kolejnej trasy

--- webpage/src/components/MainPage.js
+++ webpage/src/components/MainPage.js
@@ -44,12 +44,12 @@ export default function MainPage() {
                         <div>13:30</div>
                         <div>14:00</div>
                         <div>14:30</div>
-                        <div>15:30</div>
+                        <div className="booked">15:30</div>
                         <div>16:00</div>
                         <div>17:00</div>
                         <div>18:00</div>
                         <div>20:00</div>
-                        <div>21:00</div>
+                        <div  className="booked">21:00</div>
                         <div>22:30</div>
                         <div>23:00</div>
                         <div className="dummy"></div>
@@ -70,7 +70,64 @@ export default function MainPage() {
                     </div>
                 </div>
                 <div className="track">
-                    
+                    <div className="track-header">
+                        <div className="track-info">
+                            <div className="direction">
+                                <span>Katowice</span>
+                                <span> -----{'>'} </span>
+                                <span>Kraków</span>
+                            </div>
+                            <div className="route">
+                                <span>3 przystanki </span>
+                                <span>Balice</span>
+                                <span>-</span>
+                                <span>Chrzanów</span>
+                                <span>-</span>
+                                <span>Mysłowice</span>
+                            </div>
+                        </div>
+                        <div className="book">
+                            <p className="price">10,00 zł</p>
+                            <button className="btn-book">Rezerwuj</button>
+                        </div>
+                    </div>
+                    <div className="hours">
+                        <div>5:15</div>
+                        <div>6:00</div>
+                        <div>6:30</div>
+                        <div className="booked">7:00</div>
+                        <div>8:00</div>
+                        <div>9:00</div>
+                        <div>11:00</div>
+                        <div>12:00</div>
+                        <div>12:30</div>
+                        <div>13:30</div>
+                        <div>14:00</div>
+                        <div>14:30</div>
+                        <div>15:30</div>
+                        <div>16:00</div>
+                        <div>17:00</div>
+                        <div>18:00</div>
+                        <div>20:00</div>
+                        <div className="booked">21:00</div>
+                        <div>22:30</div>
+                        <div>23:00</div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                        <div className="dummy"></div>
+                    </div>
                 </div>
             </div>
         </div>
