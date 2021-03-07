e17b3f15200e61d077c15b399a8a1ba39d1fe46b
ania-r <80163377+ania-r@users.noreply.github.com>
2021-03-07 14:54:56 +0100

    Dodanie komponentu strony głównej

---
+++ webpage/src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+export default function MainPage() {
+    return (
+        <div className="main-page">
+            <Header />
+            <div className="main-content">
+                
+            </div>
+        </div>
+    )
+}
