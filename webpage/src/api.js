30a7272af53f18604c31f42bd823986e5696d0f7
Jakub Maniak <punisher4ce@gmail.com>
2021-03-11 12:17:04 +0100

    Poprawienie zgłaszania błędów w API

--- webpage/src/api.js
+++ webpage/src/api.js
@@ -3,19 +3,17 @@ import axios from 'axios';
 const root = '/api';
 
 async function post(path, body) {
-    try {
-        let res = await axios.post(root + path, body);
-
-        if (res.data.error) {
-            throw new Error(res.data.errorCode);
-        }
-
-        return res.data.result;
-    }
-    catch (err) {
+    let res = await axios.post(root + path, body)
+    .catch((err) => {
         console.error(err);
         throw new Error('axios_error');
+    });
+
+    if (res.data.error) {
+        throw new Error(res.data.errorCode);
     }
+
+    return res.data.result;
 }
 
 export async function login(login, password) {
