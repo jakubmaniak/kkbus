cb25e507f79bf6db58f83a5c9d2d9c588bbbda85
ania-r <80163377+ania-r@users.noreply.github.com>
2021-03-12 19:42:16 +0100

    Dodanie cennika tras

--- webpage/src/components/MainPage.js
+++ webpage/src/components/MainPage.js
@@ -25,46 +25,53 @@ export default function MainPage() {
                             </div>
                         </div>
                         <div className="book">
-                            <p className="price">10,00 zł</p>
                             <button className="btn-book">Rezerwuj</button>
                         </div>
                     </div>
-                    <div className="hours">
-                        <div>5:15</div>
-                        <div>6:00</div>
-                        <div>6:30</div>
-                        <div>7:00</div>
-                        <div>8:00</div>
-                        <div>9:00</div>
-                        <div>11:00</div>
-                        <div>12:00</div>
-                        <div>12:30</div>
-                        <div>13:30</div>
-                        <div>14:00</div>
-                        <div>14:30</div>
-                        <div className="booked">15:30</div>
-                        <div>16:00</div>
-                        <div>17:00</div>
-                        <div>18:00</div>
-                        <div>20:00</div>
-                        <div className="booked">21:00</div>
-                        <div>22:30</div>
-                        <div>23:00</div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
+                    <div className="wrapper">
+                        <div className="hours">
+                            <div>5:15</div>
+                            <div>6:00</div>
+                            <div>6:30</div>
+                            <div>7:00</div>
+                            <div>8:00</div>
+                            <div>9:00</div>
+                            <div>11:00</div>
+                            <div>12:00</div>
+                            <div>12:30</div>
+                            <div>13:30</div>
+                            <div>14:00</div>
+                            <div>14:30</div>
+                            <div className="booked">15:30</div>
+                            <div>16:00</div>
+                            <div>17:00</div>
+                            <div>18:00</div>
+                            <div>20:00</div>
+                            <div className="booked">21:00</div>
+                            <div>22:30</div>
+                            <div>23:00</div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                        </div>
+                        <div className="prices">
+                            <p>Normalny: 15zł</p>
+                            <p>Ulgowy*: 10.5zł</p>
+                            <p>Dzieci do lat 5: bezpłatnie</p>
+                            <p className="note">*studenci i uczniowie</p>
+                        </div>
                     </div>
                 </div>
                 <div className="track">
@@ -85,46 +92,53 @@ export default function MainPage() {
                             </div>
                         </div>
                         <div className="book">
-                            <p className="price">10,00 zł</p>
                             <button className="btn-book">Rezerwuj</button>
                         </div>
                     </div>
-                    <div className="hours">
-                        <div>5:15</div>
-                        <div>6:00</div>
-                        <div>6:30</div>
-                        <div className="booked">7:00</div>
-                        <div>8:00</div>
-                        <div>9:00</div>
-                        <div>11:00</div>
-                        <div>12:00</div>
-                        <div>12:30</div>
-                        <div>13:30</div>
-                        <div>14:00</div>
-                        <div>14:30</div>
-                        <div>15:30</div>
-                        <div>16:00</div>
-                        <div>17:00</div>
-                        <div>18:00</div>
-                        <div>20:00</div>
-                        <div className="booked">21:00</div>
-                        <div>22:30</div>
-                        <div>23:00</div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
-                        <div className="dummy"></div>
+                    <div className="wrapper">
+                        <div className="hours">
+                            <div>5:15</div>
+                            <div>6:00</div>
+                            <div>6:30</div>
+                            <div className="booked">7:00</div>
+                            <div>8:00</div>
+                            <div>9:00</div>
+                            <div>11:00</div>
+                            <div>12:00</div>
+                            <div>12:30</div>
+                            <div>13:30</div>
+                            <div>14:00</div>
+                            <div>14:30</div>
+                            <div>15:30</div>
+                            <div>16:00</div>
+                            <div>17:00</div>
+                            <div>18:00</div>
+                            <div>20:00</div>
+                            <div className="booked">21:00</div>
+                            <div>22:30</div>
+                            <div>23:00</div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                            <div className="dummy"></div>
+                        </div>
+                        <div className="prices">
+                            <p>Normalny: 12zł</p>
+                            <p>Ulgowy*: 8.4zł</p>
+                            <p>Dzieci do lat 5: bezpłatnie</p>
+                            <p className="note">*studenci i uczniowie</p>
+                        </div>
                     </div>
                 </div>
             </div>
