73be08e1a23d9772eb99d4c6408757dfab35ab9c
ania-r <80163377+ania-r@users.noreply.github.com>
2021-03-13 18:16:45 +0100

    Dodanie komponentu grafiku pracy

--- webpage/src/components/WorkSchedule.js
+++ webpage/src/components/WorkSchedule.js
@@ -1,9 +1,66 @@
 import React from 'react';
 import '../styles/WorkSchedule.css';
 import WorkScheduleFilter from './WorkScheduleFilter';
+import WorkScheduleItem from './WorkScheduleItem';
 
 function WorkSchedule() {
     let drivers = ['Tomasz Rajdowiec', 'Kazimierz Rajdowiec', 'Mirosław Szybki'];
+    let data = [
+        {
+            start: 'Kraków',
+            end: 'Katowice',
+            day: 'dzisiaj',
+            hour: '13:30',
+            vehicle: 'Mercedes Sprinter (KR 193PK)',
+            parking: 'Kraków, parking Czyżyny, ',
+            parkingInfo: 'początkowy'
+        },
+        {
+            start: 'Katowice',
+            end: 'Kraków',
+            day: 'dzisiaj',
+            hour: '15:15',
+            vehicle: 'Mercedes Sprinter (KR 193PK)',
+            parking: '-',
+            parkingInfo: ''
+        },
+        {
+            start: 'Kraków',
+            end: 'Katowice',
+            day: 'dzisiaj',
+            hour: '17:00',
+            vehicle: 'Mercedes Sprinter (KR 193PK)',
+            parking: '-',
+            parkingInfo: ''
+        },
+        {
+            start: 'Katowice',
+            end: 'Kraków',
+            day: 'dzisiaj',
+            hour: '18:45',
+            vehicle: 'Mercedes Sprinter (KR 193PK)',
+            parking: '-',
+            parkingInfo: ''
+        },
+        {
+            start: 'Kraków',
+            end: 'Katowice',
+            day: 'dzisiaj',
+            hour: '20:30',
+            vehicle: 'Mercedes Sprinter (KR 193PK)',
+            parking: '-',
+            parkingInfo: ''
+        },
+        {
+            start: 'Katowice',
+            end: 'Kraków',
+            day: 'dzisiaj',
+            hour: '22:15',
+            vehicle: 'Mercedes Sprinter (KR 193PK)',
+            parking: 'Kraków, parking Czyżyny, ',
+            parkingInfo: 'końcowy'
+        }
+    ]
 
     return (
         <div className="work-schedule-page">
@@ -14,7 +71,7 @@ function WorkSchedule() {
                         <WorkScheduleFilter label="Kierowca:">
                             <div className="list-item">{drivers[0]}</div>
                             {drivers.map((driver) => {
-                                if(driver != drivers[0]) {
+                                if(driver !== drivers[0]) {
                                     return (
                                         <div className="list-item hidden">{driver}</div>
                                     );
@@ -28,6 +85,19 @@ function WorkSchedule() {
                         <WorkScheduleFilter label="Kierunki:"/>
                     </div>
                 </div>
+                {data.map((element) => {
+                    return (
+                        <WorkScheduleItem 
+                            start={element.start}
+                            end={element.end}
+                            day={element.day}
+                            hour={element.hour}
+                            vehicle={element.vehicle}
+                            parking={element.parking}
+                            parkingInfo={element.parkingInfo}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
