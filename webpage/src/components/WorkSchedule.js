d801c9a277e47c362c916463fcf07d2cae90b103
ania-r <80163377+ania-r@users.noreply.github.com>
2021-03-13 17:23:04 +0100

    Dodanie komponentu filtr

--- webpage/src/components/WorkSchedule.js
+++ webpage/src/components/WorkSchedule.js
@@ -1,80 +1,32 @@
 import React from 'react';
 import '../styles/WorkSchedule.css';
+import WorkScheduleFilter from './WorkScheduleFilter';
 
 function WorkSchedule() {
-    function selectItem() {
-        let items = document.querySelectorAll('.hidden');
-        items.forEach(item => {
-            item.classList.remove('hidden');
-            item.classList.add('shown');
-        });
-    }
-    
+    let drivers = ['Tomasz Rajdowiec', 'Kazimierz Rajdowiec', 'Mirosław Szybki'];
+
     return (
         <div className="work-schedule-page">
             <div className="main">
                 <div className="tile">
                     <h2>Filtry</h2>
                     <div className="row-container">
-                        <div className="list-container">
-                            <span>Kierowca:</span>
-                            <div className="drop-down-list">
-                                <div className="list-items-container">
-                                    <div className="list-items">
-                                        <div className="list-item">Tomasz Rajdowiec</div>
-                                        <div className="list-item hidden">Tomasz Rajdowiec</div>
-                                        <div className="list-item hidden">Tomasz Rajdowiec</div>
-                                        <div className="list-item hidden">Tomasz Rajdowiec</div>
-                                    </div>
-                                </div>
-                                <button className="list-button" onClick={selectItem}>v</button>
-                            </div>
-                        </div>
-                        <div className="list-container">
-                            <span>Trasy:</span>
-                            <div className="drop-down-list">
-                                <div className="list-items-container">
-                                    <div className="list-items">
-                                        <div className="list-item">wszystkie</div>
-                                        <div className="list-item hidden">wszystkie</div>
-                                        <div className="list-item hidden">wszystkie</div>
-                                        <div className="list-item hidden">wszystkie</div>
-                                    </div>
-                                </div>
-                                <button className="list-button" onClick={selectItem}>v</button>
-                            </div>
-                        </div>
+                        <WorkScheduleFilter label="Kierowca:">
+                            <div className="list-item">{drivers[0]}</div>
+                            {drivers.map((driver) => {
+                                if(driver != drivers[0]) {
+                                    return (
+                                        <div className="list-item hidden">{driver}</div>
+                                    );
+                                }
+                            })}
+                        </WorkScheduleFilter>
+                        <WorkScheduleFilter label="Trasy:"/>
                     </div>
-                   <div className="row-container">
-                        <div className="list-container">
-                                <span>Zakres dni:</span>
-                                <div className="drop-down-list">
-                                    <div className="list-items-container">
-                                        <div className="list-items">
-                                            <div className="list-item">dzisiaj, jutro</div>
-                                            <div className="list-item hidden">dzisiaj, jutro</div>
-                                            <div className="list-item hidden">dzisiaj, jutro</div>
-                                            <div className="list-item hidden">dzisiaj, jutro</div>
-                                        </div>
-                                    </div>
-                                    <button className="list-button" onClick={selectItem}>v</button>
-                                </div>
-                            </div>
-                            <div className="list-container">
-                                <span>Kierunki:</span>
-                                <div className="drop-down-list">
-                                    <div className="list-items-container">
-                                        <div className="list-items">
-                                            <div className="list-item">wszystkie</div>
-                                            <div className="list-item hidden">wszystkie</div>
-                                            <div className="list-item hidden">wszystkie</div>
-                                            <div className="list-item hidden">wszystkie</div>
-                                        </div>
-                                    </div>
-                                    <button className="list-button" onClick={selectItem}>v</button>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="row-container">
+                        <WorkScheduleFilter label="Zakres dni:"/>
+                        <WorkScheduleFilter label="Kierunki:"/>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ webpage/src/components/WorkScheduleFilter.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../styles/WorkSchedule.css';
+
+function WorkScheduleFilter(props) {
+    function selectItem(ev) {
+        let listContainer = ev.currentTarget.parentNode;
+        let items = listContainer.querySelectorAll('.hidden');
+
+        items.forEach(item => {
+            item.classList.remove('hidden');
+            item.classList.add('shown');
+        });
+    }
+
+    return (
+        <div className="list-container">
+            <span>{props.label}</span>
+            <div className="drop-down-list" onClick={(ev) => selectItem(ev)}>
+                <div className="list-items-container">
+                    <div className="list-items">
+                        {props.children}
+                        {/* <div className="list-item">Tomasz Rajdowiec</div>
+                        <div className="list-item hidden">Tomasz Rajdowiec</div>
+                        <div className="list-item hidden">Tomasz Rajdowiec</div>
+                        <div className="list-item hidden">Tomasz Rajdowiec</div> */}
+                    </div>
+                </div>
+                <button className="list-button">v</button>
+            </div>
+        </div>
+    );
+}
+
+export default WorkScheduleFilter;
