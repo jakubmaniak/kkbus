2b9a0374f6905b70edc910708076bb5b97c70146
ania-r <80163377+ania-r@users.noreply.github.com>
2021-03-14 22:57:19 +0100

    Dodanie przycisku edytowania kontaktu

--- webpage/src/components/Contact.js
+++ webpage/src/components/Contact.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import '../styles/Contact.css';
 import map from '../static/map.png';
 
+import UserContext from '../contexts/User';
+
 function Contact() {
+    let { role } = useContext(UserContext).user;
+
     return (
         <div className="contact page">
             <div className="main">
                 <div className="contact-container">
                     <div className="tile half">
-                        <h2>Kontakt</h2>
+                        <div className="header-wrapper">
+                            <h2>Kontakt</h2>
+                            {role === 'owner' ? 
+                                <button className="edit">Edytuj</button> 
+                            : null}
+                        </div>
                         <div className="adress">
                             <p>ul. Jana Pawła II 37</p>
                             <p>31-864 Kraków</p>
