35306a8611db1f9c03a0941238b172a2df9b6ef1
ania-r <80163377+ania-r@users.noreply.github.com>
2021-03-17 14:09:05 +0100

    Dodanie opcji edycji pojazdów

--- webpage/src/components/Vehicle.js
+++ webpage/src/components/Vehicle.js
@@ -1,12 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import '../styles/VehicleInfo.css';
 import Modal from './Modal';
+import UserContext from '../contexts/User';
+import Dropdown from './Dropdown';
+import DropdownMultiple from './DropdownMultiple';
 
 function Vehicle(props) {
-    let [modalVisibility, setModalVisibility] = useState(false);
+    let { role } = useContext(UserContext).user;
+    let [modalDetailsVisibility, setModalDetailsVisibility] = useState(false);
+    let [modalEditDataVisibility, setModalEditDataVisibility] = useState(false);
+    let [modalChangeDriverVisibility, setModalChangeDriverVisibility] = useState(false);
 
-    function showModal() {
-        setModalVisibility(true);
+    function editDataModal() {
+        return (
+            <Modal visible={modalEditDataVisibility}>
+                <header>Edycja danych pojazdu</header>
+                <section className="content">
+                    <form className="vehicle-edit">
+                        <div className="input-container"> 
+                            <input placeholder="Marka"/>
+                            <input placeholder="Model"/>
+                        </div>
+                       <div className="input-container">
+                            <input placeholder="Rocznik"/>
+                            <input placeholder="Przebieg"/>
+                       </div>
+                        <div className="input-container">
+                            <input placeholder="Rejestracja"/>
+                            <input placeholder="Ilość miejsc"/>
+                        </div>
+                        <Dropdown placeholder="Miejsce stałego parkowania"/>
+                        <DropdownMultiple placeholder="Dostępne trasy dla pojazdów"/>
+                    </form>
+                </section>
+                <section className="footer">
+                    <button onClick={() => setModalEditDataVisibility(false)}>Anuluj</button>
+                    <button onClick={() => setModalEditDataVisibility(false)}>Zapisz</button>
+                </section>
+            </Modal>
+        );
+    }
+
+    function changeDriverModal() {
+        return (
+            <Modal visible={modalChangeDriverVisibility}>
+                <header>Zmiana kierowcy</header>
+                <section className="content">
+                        <Dropdown placeholder="Aktualny kierowca"/>
+                </section>
+                <section className="footer">
+                    <button onClick={() => setModalChangeDriverVisibility(false)}>Anuluj</button>
+                    <button onClick={() => setModalChangeDriverVisibility(false)}>Zapisz</button>
+                </section>
+            </Modal>
+        );
     }
 
     return (
@@ -29,10 +76,21 @@ function Vehicle(props) {
                     </span>
                 </div>
                 <div className="details">
-                    <button onClick={showModal}>Szczegóły</button>
+                    <button onClick={() => setModalDetailsVisibility(true)}>Szczegóły</button>
+                    {role === 'owner' ? 
+                        <div className="vehicle-edit-button">
+                            <button onClick={() => {setModalEditDataVisibility(true)}}>Edytuj dane</button>
+                            <button onClick={() => {setModalChangeDriverVisibility(true)}}>Zmień kierowcę</button>
+                            <button className="delete">Usuń</button>
+                        </div>   
+                    : role === 'office' ?
+                    <div className="vehicle-edit-button">
+                        <button onClick={() => {setModalChangeDriverVisibility(true)}}>Zmień kierowcę</button>
+                    </div>
+                    : null}
                 </div>
             </div>
-            <Modal visible={modalVisibility}>
+            <Modal visible={modalDetailsVisibility}>
                 <header>Szczegóły pojazdu</header>
                 <section className="content">
                     <div className="vehicle-info">
@@ -51,11 +109,23 @@ function Vehicle(props) {
                         <span>Średnie spalanie:</span>
                         <span>{props.avgCombustion}</span>
                     </div>
+                    <div className="vehicle-info">
+                        <span>Aktualny kierowca:</span>
+                        <span>{props.currentDriver}</span>
+                    </div>
                 </section>
                 <section className="footer">
-                    <button onClick={() => setModalVisibility(false)}>Zamknij</button>
+                    <button onClick={() => setModalDetailsVisibility(false)}>Zamknij</button>
                 </section>
             </Modal>
+            {/* {role === 'owner' ?
+                [
+                    editDataModal(), 
+                    changeDriverModal()
+                ]
+            : null} */}
+            {role === 'owner' ? editDataModal() : null}
+            {role === 'owner' || role === 'office' ? changeDriverModal() : null} 
         </div>
     );
 }
--- webpage/src/components/VehicleInfo.js
+++ webpage/src/components/VehicleInfo.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../styles/VehicleInfo.css';
 import Vehicle from './Vehicle';
 
-function VehicleInfo() {
+function VehicleInfo() {    
     let vehicles = [
         {
             vehicleRegistration: 'KR 111 AB',
@@ -13,7 +13,8 @@ function VehicleInfo() {
             returnTrack: 'Katowice - Kraków',
             seats: 22,
             vehicleMileage: 10000000,
-            avgCombustion: 8.9
+            avgCombustion: 8.9,
+            currentDriver: 'Tomasz Rajdowiec'
         },
         {
             vehicleRegistration: 'KR 124 AO',
@@ -24,40 +25,44 @@ function VehicleInfo() {
             returnTrack: 'Katowice - Kraków',
             seats: 18,
             vehicleMileage: 900000,
-            avgCombustion: 9
+            avgCombustion: 9,
+            currentDriver: 'Kazimierz Rajdowiec'
         },
         {
             vehicleRegistration: 'KR 875 CF',
             name: 'Iveco Daily 2014',
             state: 'Aktywny',
-            parking: 'Parking nr 1',
+            parking: 'Parking nr 2',
             oneWayTrack: 'Kraków - Katowice',
             returnTrack: 'Katowice - Kraków',
             seats: 22,
             vehicleMileage: 9.5,
-            avgCombustion: 80500300
+            avgCombustion: 80500300,
+            currentDriver: 'Mirosław Szybki'
         },
         {
             vehicleRegistration: 'KR 222 KM',
             name: 'Fiat Ducato 2014',
             state: 'Aktywny',
-            parking: 'Parking nr 1',
+            parking: 'Parking nr 2',
             oneWayTrack: 'Kraków - Katowice',
             returnTrack: 'Katowice - Kraków',
             seats: 15,
             vehicleMileage: 9.4,
-            avgCombustion: 10540003
+            avgCombustion: 10540003,
+            currentDriver: 'Jan Doświadczony'
         },
         {
             vehicleRegistration: 'KR 990 JB',
             name: 'Scania Touring 2015',
             state: 'Aktywny',
-            parking: 'Parking nr 1',
+            parking: 'Parking nr 2',
             oneWayTrack: 'Kraków - Katowice',
             returnTrack: 'Katowice - Kraków',
             seats: 55,
             vehicleMileage: 7.9,
-            avgCombustion: 9023305
+            avgCombustion: 9023305,
+            currentDriver: 'Marek Poprawny'
         }
     ];
 
@@ -76,6 +81,7 @@ function VehicleInfo() {
                             returnTrack={vehicle.returnTrack}
                             vehicleMileage={vehicle.vehicleMileage}
                             avgCombustion={vehicle.avgCombustion}
+                            currentDriver={vehicle.currentDriver}
                         />
                     );
                 })}
