a98dd33efa97cc13dba739964a993acbba5079c9
Jakub Maniak <punisher4ce@gmail.com>
2021-03-21 11:20:18 +0100

    Dodanie domyślnych wartości do pliku konfiguracyjnego

--- webserver/helpers/env.js
+++ webserver/helpers/env.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
 const env = JSON.parse(fs.readFileSync('env.json'));
 
-module.exports = env;
+module.exports = {
+    ...env,
+    server: {
+        host: env.host || 'localhost',
+        port: env.port || 3200
+    },
+    jwtSecret: env.jwtSecret || 'kkbus_jwt_secret',
+    mysql: {
+        ...env.mysql,
+        port: env.mysql.port || 3306
+    }
+};
