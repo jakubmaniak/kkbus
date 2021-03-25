ba0566e768beca1847015e892f21f912f80bc571
ania-r <80163377+ania-r@users.noreply.github.com>
2021-03-25 16:56:47 +0100

    Dodanie pobierania tras z serwera

--- webpage/src/components/MainPage.js
+++ webpage/src/components/MainPage.js
@@ -1,152 +1,96 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/MainPage.css';
 import Track from './Track';
+import * as api from '../api';
 
 export default function MainPage() {
-    let [tracks, setTracks] = useState(
-        [
-            {//dane z bazy dla jednej trasy (kursu)
-                startingStop: 'Katowice',
-                finalStop: 'Kraków',
-                busStops: ['Katowice', 'Chrznów', 'Kraków Dworzec Główny'],
-                hours: ['5:15','6:00','6:30','7:00','8:00','9:00','11:00','12:00','12:30','13:30','14:00','14:30','16:00','17:00','18:00','20:00','22:30','23:00'],
-                prices: [
-                    {
-                        busStop: 'Kraków - Chrzanów',
-                        normalPrice: 5, 
-                        reducedPrice: 3.5
-                    },
-                    {
-                        busStop: 'Chrzanów - Kraków',
-                        normalPrice: 5, 
-                        reducedPrice: 3.5
-                    },
-                    {
-                        busStop: 'Katowice - Kraków',
-                        normalPrice: 12, 
-                        reducedPrice: 8.4
-                    }
-                ]
-            },
-            {
-                startingStop: 'Kraków',
-                finalStop: 'Katowice',
-                busStops: ['Kraków Dworzec Główny', 'Chrznów', 'Jaworzno', 'Katowice'],
-                hours: ['5:15','6:00','6:30','7:00','8:00','9:00','11:00','12:00','12:30','13:30','14:00','14:30','16:00','17:00','18:00','20:00','22:30','23:00'],
-                prices: [
-                    {
-                        busStop: 'Kraków - Chrzanów',
-                        normalPrice: 4, 
-                        reducedPrice: 2.8
-                    },
-                    {
-                        busStop: 'Chrzanów - Jaworzno',
-                        normalPrice: 6, 
-                        reducedPrice: 4.2
-                    },
-                    {
-                        busStop: 'Kraków - Jaworzno',
-                        normalPrice: 12, 
-                        reducedPrice: 8.4
-                    },
-                    {
-                        busStop: 'Kraków - Katowice',
-                        normalPrice: 15, 
-                        reducedPrice: 10.4
-                    }
-                ]
-            },
-            {
-                startingStop: 'Kraków',
-                finalStop: 'Warszawa',
-                busStops: ['Kraków Dworzec Główny', 'Kielce', 'Radom', 'Warszawa Centralna'],
-                hours: ['5:15','6:00','6:30','7:00','8:00','9:00','11:00','12:00','12:30','13:30','14:00','14:30','16:00','17:00','18:00','20:00','22:30','23:00'],
-                prices: [
-                    {
-                        busStop: 'Kraków - Kielce',
-                        normalPrice: 5, 
-                        reducedPrice: 3.5
-                    },
-                    {
-                        busStop: 'Kielce - Radom',
-                        normalPrice: 5, 
-                        reducedPrice: 3.5
-                    },
-                    {
-                        busStop: 'Kraków - Radom',
-                        normalPrice: 12, 
-                        reducedPrice: 8.4
-                    },
-                    {
-                        busStop: 'Kraków - Warszawa',
-                        normalPrice: 12, 
-                        reducedPrice: 8.4
-                    }
-                ]
-            },
-            {
-                startingStop: 'Warszawa',
-                finalStop: 'Kraków',
-                busStops: ['Warszawa Centralna', 'Radom', 'Kielce', 'Kraków Dworzec Główny'],
-                hours: ['5:15','6:00','6:30','7:00','8:00','9:00','11:00','12:00','12:30','13:30','14:00','14:30','16:00','17:00','18:00','20:00','22:30','23:00'],
-                prices: [
-                    {
-                        busStop: 'Warszawa - Radom',
-                        normalPrice: 5, 
-                        reducedPrice: 3.5
-                    },
-                    {
-                        busStop: 'Radom - Kielce',
-                        normalPrice: 5, 
-                        reducedPrice: 3.5
-                    },
-                    {
-                        busStop: 'Warszawa - Kielce',
-                        normalPrice: 12, 
-                        reducedPrice: 8.4
-                    },
-                    {
-                        busStop: 'Warszawa - Kraków',
-                        normalPrice: 12, 
-                        reducedPrice: 8.4
-                    }
-                ]
-            },
-        ]
-    );
+    let [tracks, setTracks] = useState([]);
+    let [a, setA] = useState([]);
+
+    useEffect(() => {
+        api.getAllRoutes()
+        .then((results) => {
+            setTracks(results);
+        });
+    }, []);
+
+
+    function stopsPrice(stops) {
+        let arr=[];
+
+        for(let i = 0; i < stops.length - 1; i++) {
+            for(let j = 0; j < stops.length - 1; j++) {
+                if(stops[j] !== stops[i + 1] && stops[j] < stops[i + 1]) {
+                    console.log(stops[j], stops[i + 1]);
+                    arr.push((stops[j] - stops[i + 1]));
+                }
+            }
+        }
+
+        setA(arr);
+    }
 
     return (
         <div className="main-page">
             <div className="main">
                 {tracks.map((track, i) => {
                     return (
+                        <div key={i}>
                         <Track
-                            key={i}
-                            startingStop={track.startingStop}
-                            finalStop={track.finalStop}
-                            busStops={track.busStops.map((busStop, i) => {
+                            key={track.id + 'a'}
+                            startingStop={track.a.departureLocation}
+                            finalStop={track.b.departureLocation}
+                            busStops={track.a.stops.map((stop, i) => {
                                 return (
                                     <span className="route" key={i}>
-                                        <span>{busStop}</span>
-                                        {i < track.busStops.length - 1 ? <span> - </span> : null}
+                                        <span>{stop}</span>
+                                        {i < track.a.stops.length - 1 ? <span> - </span> : null}
                                     </span>
                                 );
                             })}
-                            hours={track.hours.map((hour, i) => {
+                            hours={track.a.hours.map((hour, i) => {
                                 return (
                                     <div key={i}>{hour}</div>
                                 );
                             })}
-                            prices={track.prices.map((price, i) => {
-                                return (
-                                    <div className="bus-stop-price" key={i}>
-                                        <span className="bus-stop">{price.busStop}</span>
-                                        <span className="price">{price.normalPrice}zł</span>
-                                        <span className="price">{price.reducedPrice}zł</span>
-                                    </div>
-                                );
+                            prices={track.a.prices.map((price, i) => {
+                                    return (
+                                        <div className="bus-stop-price" key={i}>
+                                            <span className="bus-stop"> - </span>
+                                            <span className="price">{price}zł</span>
+                                            <span className="price">{price - price * 0.3}zł</span>
+                                        </div>
+                                    );
                             })}
                         />
+                        <Track
+                            key={track.id + 'b'}
+                            startingStop={track.b.departureLocation}
+                            finalStop={track.a.departureLocation}
+                            busStops={track.b.stops.map((stop, i) => {
+                                return (
+                                    <span className="route" key={i}>
+                                        <span>{stop}</span>
+                                        {i < track.b.stops.length - 1 ? <span> - </span> : null}
+                                    </span>
+                                );
+                            })}
+                            hours={track.b.hours.map((hour, i) => {
+                                return (
+                                    <div key={i}>{hour}</div>
+                                );
+                            })}
+                            prices={track.b.prices.map((price, i) => {
+                                    return (
+                                        <div className="bus-stop-price" key={i}>
+                                            <span className="bus-stop"> - </span>
+                                            <span className="price">{price}zł</span>
+                                            <span className="price">{price - price * 0.3}zł</span>
+                                        </div>
+                                    );
+                            })}
+                        />
+                        </div>
                     );
                 })}
             </div>
