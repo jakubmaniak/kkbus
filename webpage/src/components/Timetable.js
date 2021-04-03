73fbd94dfb30c11279dbac5f3a7799f67ec9efc8
ania-r <80163377+ania-r@users.noreply.github.com>
2021-04-03 22:27:43 +0200

    Dodanie komponentu zawierającego wybrane dni

--- webpage/src/components/Timetable.js
+++ webpage/src/components/Timetable.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React from 'react';
+import Dropdown from './Dropdown';
+import '../styles/Timetable.css';
+import TimetableFilterDays from './TimetableFilterDays';
+
+let filterDays = ['sb, 03.04', 'nd, 04.04', 'p, 505.04', 'wt, 06.04', 'śr, 07.04', 'cz, 08.04', 'pt, 09.04'];
 
 function Timetable() {
     return (
         <div className="timetable page">
             <div className="main">
-                
+                <div className="tile">
+                    <h2>Filtry</h2>
+                    <div className="filters">
+                        <div className="filter-container">
+                            <span>Profesja:</span>
+                            <Dropdown placeholder="kierowca, sekretariat"/>
+                        </div>
+                        <div className="filter-container">
+                            <span>Zakres:</span>
+                            <Dropdown placeholder="3.04-9.04"/>
+                        </div>
+                    </div>
+                </div>
+               <TimetableFilterDays 
+                    children={filterDays.map((filterDay, i) => {
+                        return (
+                            <span key={i}>{filterDay}</span>
+                        )
+                    })}
+               />
             </div>
         </div>
-    )
+    );
 }
 
-export default Timetable
+export default Timetable;
