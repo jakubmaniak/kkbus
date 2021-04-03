6df594a5456e3619c9b724b42d76d1e092653a1c
ania-r <80163377+ania-r@users.noreply.github.com>
2021-04-03 21:58:54 +0200

    Dodanie komponentu harmonogramu

---
+++ webpage/src/components/Timetable.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Timetable() {
+    return (
+        <div className="timetable page">
+            <div className="main">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Timetable
