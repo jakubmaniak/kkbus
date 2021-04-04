9a6c0434ef63c09d3196fc64bb2d61cccc0ec8d5
Jakub Maniak <punisher4ce@gmail.com>
2021-04-04 19:06:40 +0200

    Usunięcie zbędnych linijek kodu

--- webserver/routes/route.js
+++ webserver/routes/route.js
@@ -14,51 +14,9 @@ const role = require('../middlewares/roles')(
 const routeController = require('../controllers/route');
 
 
-const routes = new Map([
-    [1, {
-        id: 1,
-        oppositeId: 2,
-        departureLocation: 'Kraków',
-        arrivalLocation: 'Katowice',
-        prices: [4.00, 4.00, 4.00],
-        stops: ['Kraków Dworzec Główny', 'Chrzanów', 'Jaworzno', 'Katowice'],
-        hours: ['5:15', '6:00', '6:30', '7:00', '8:00', '9:00', '11:00', '12:00', '12:30', '13:30', '14:00', '14:30', '16:00', '17:00', '18:00', '20:00', '22:30', '23:00']
-    }],
-    [2, {
-        id: 2,
-        oppositeId: 1,
-        departureLocation: 'Katowice',
-        arrivalLocation: 'Kraków',
-        prices: [6.00, 6.00],
-        stops: ['Katowice', 'Chrzanów', 'Kraków Dworzec Główny'],
-        hours: ['5:15', '6:00', '6:30', '7:00', '8:00', '9:00', '11:00', '12:00', '12:30', '13:30', '14:00', '14:30', '16:00', '17:00', '18:00', '20:00', '22:30', '23:00']
-    }],
-    [3, {
-        id: 3,
-        oppositeId: 4,
-        departureLocation: 'Kraków',
-        arrivalLocation: 'Warszawa',
-        prices: [4.00, 5.00, 5.00],
-        stops: ['Kraków Dworzec Główny', 'Kielce', 'Radom', 'Warszawa Centralna'],
-        hours: ['5:15', '6:00', '6:30', '7:00', '8:00', '9:00', '11:00', '12:00', '12:30', '13:30', '14:00', '14:30', '16:00', '17:00', '18:00', '20:00', '22:30', '23:00']
-    }],
-    [4, {
-        id: 4,
-        oppositeId: 3,
-        departureLocation: 'Warszawa',
-        arrivalLocation: 'Kraków',
-        prices: [5.00, 5.00, 4.00],
-        stops: ['Warszawa Centralna', 'Radom', 'Kielce', 'Kraków Dworzec Główny'],
-        hours: ['5:15', '6:00', '6:30', '7:00', '8:00', '9:00', '11:00', '12:00', '12:30', '13:30', '14:00', '14:30', '16:00', '17:00', '18:00', '20:00', '22:30', '23:00']
-    }]
-]);
-
-
 router.get('/routes', async (req, res, next) => {
     res.ok(await routeController.findAllRoutes());
-    /*res.ok(
-        [...routes].map(([id, route]) => ({ id, ...route }))
-    );*/
+
 });
 
 router.get('/route/:id', async (req, res, next) => {
@@ -87,8 +45,6 @@ router.get('/route/:id', async (req, res, next) => {
     catch (err) {
         next(err);
     }
-    //  res.ok(routes.get(routeId));
-    // }
 });
 
 router.post('/route', [
