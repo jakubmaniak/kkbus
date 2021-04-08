32b95b2502c8b7a595b5c371524452d46182465e
ania-r <80163377+ania-r@users.noreply.github.com>
2021-04-08 20:33:01 +0200

    Dodanie stylów dla okna powiadomienia i drobne jego poprawki

--- webpage/src/components/NotificationModal.js
+++ webpage/src/components/NotificationModal.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Modal from './Modal';
+import '../styles/NotificationModal.css';
 
 function NotificationModal(props) {
     return (
-        <Modal visible={props.visible}>
-            <header>{props.header}</header>
-            <section className="content">
-                <p>Czy na pewno chcesz usunąć {props.name}?</p>
-            </section>
-            <section className="footer">
-                <div>
-                    <button onClick={props.notificationModalExit}>Anuluj</button>
-                    <button className="delete" onClick={props.delete}>Tak, usuń</button>
-                </div>
-            </section>
-        </Modal>
+        <div className="notification-modal">
+            <Modal visible={props.visible}>
+                <header>{props.header}</header>
+                <section className="content">
+                    <p>Czy na pewno chcesz usunąć {props.name}?</p>
+                </section>
+                <section className="footer">
+                    <div>
+                        <button onClick={props.notificationModalExit}>Anuluj</button>
+                        <button className="delete" onClick={props.delete}>Tak, usuń</button>
+                    </div>
+                </section>
+            </Modal>
+        </div>
+        
     )
 }
 
