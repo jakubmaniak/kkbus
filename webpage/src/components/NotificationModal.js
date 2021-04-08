e722ce0e00882fc5aa8e4fad507209e0ffb1ec8f
ania-r <80163377+ania-r@users.noreply.github.com>
2021-04-08 20:08:06 +0200

    Poprawienie funkcjonowania okna powiadomień

--- webpage/src/components/NotificationModal.js
+++ webpage/src/components/NotificationModal.js
@@ -3,18 +3,18 @@ import Modal from './Modal';
 
 function NotificationModal(props) {
     return (
-        <Modal visible={modalDeleteRewardVisibility}>
-                <header>{props.header}</header>
-                <section className="content">
-                    <p>Czy na pewno chcesz usunąć {props.name}?</p>
-                </section>
-                <section className="footer">
-                    <div>
-                        <button onClick={props.modalVisibility}>Anuluj</button>
-                        <button className="delete" onClick={props.delete}>Tak, usuń</button>
-                    </div>
-                </section>
-            </Modal>
+        <Modal visible={props.visible}>
+            <header>{props.header}</header>
+            <section className="content">
+                <p>Czy na pewno chcesz usunąć {props.name}?</p>
+            </section>
+            <section className="footer">
+                <div>
+                    <button onClick={props.notificationModalExit}>Anuluj</button>
+                    <button className="delete" onClick={props.delete}>Tak, usuń</button>
+                </div>
+            </section>
+        </Modal>
     )
 }
 
