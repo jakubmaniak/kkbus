721938e1242009ef7968cf1385b11e63fed41904
aburdzy <80163377+aburdzy@users.noreply.github.com>
2021-05-01 18:38:55 +0200

    Dodanie obsługi zapisywania raportu z kursu

--- webpage/src/components/RouteReport.js
+++ webpage/src/components/RouteReport.js
@@ -4,13 +4,20 @@ import '../styles/RouteReport.css';
 import * as api from '../api';
 import { routeFormatter } from '../helpers/text-formatters';
 import UserContext from '../contexts/User';
+import toast from '../helpers/toast';
+import { fromValue } from '../helpers/from-value';
 
 import Dropdown from './Dropdown';
-
 
 function RouteReport() {
     let [routes, setRoutes] = useState([]);
     let [stops, setStops] = useState([]);
+    let [vehicles, setVehicles] = useState([]);
+
+    let [selectedRoute, setSelectedRoute] = useState();
+    let [selectedStop, setSelectedStop] = useState();
+    let [selectedVehicle, setSelectedVehicle] = useState();
+    let [amount, setAmount] = useState('');
 
     let { role } = useContext(UserContext).user;
 
@@ -18,10 +25,34 @@ function RouteReport() {
         api.getAllRoutes()
             .then(setRoutes)
             .catch(api.toastifyError);
+
+        api.getAllVehicles()
+            .then(setVehicles)
+            .catch(api.toastifyError);
     }, []);
 
     function handleRouteChange(route) {
         setStops(route.stops);
+        setSelectedRoute(route);
+    }
+
+    function saveReport() {
+        let currentAmount = parseInt(amount, 10);
+        
+        if(isNaN(amount) || currentAmount < 0) {
+            toast.error('Wprowadzono niepoprawną liczbę osób');
+            return;
+        }
+        if(!selectedRoute || !selectedStop || !selectedVehicle) {
+            toast.error('Wprowadzone dane są niepoprawne');
+            return;
+        }
+        
+        console.log(selectedRoute.id, selectedStop, selectedVehicle.id, currentAmount);
+
+        api.addRouteReport(selectedRoute.id, selectedStop, selectedVehicle.id, currentAmount)
+        .then(() => toast.success('Dodano raport'))
+        .catch(api.toastifyError);
     }
 
     return (
@@ -37,12 +68,21 @@ function RouteReport() {
                             items={routes}
                             textFormatter={routeFormatter}
                             placeholder="Wybierz trasę"
-                            handleChange={handleRouteChange} />
+                            handleChange={handleRouteChange} 
+                        />
                         <Dropdown
                             items={stops}
-                            placeholder="Wybierz przystanek" />
-                        <input placeholder="Liczba osób" />
-                        <button className="submit">Zapisz raport</button>
+                            placeholder="Wybierz przystanek" 
+                            handleChange={setSelectedStop}
+                        />
+                        <Dropdown
+                            items={vehicles}
+                            textFormatter={({ brand, model, year }) => brand + ' ' + model + ' ' + year}
+                            placeholder="Wybierz pojazd" 
+                            handleChange={setSelectedVehicle} 
+                        />
+                        <input placeholder="Liczba osób" value={amount} onChange={fromValue(setAmount)} />
+                        <button className="submit" type="button" onClick={saveReport}>Zapisz raport</button>
                     </form>
                 </div>
             </div>
