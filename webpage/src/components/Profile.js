f1a784f2fed06b9f25792249346389400eaf4ebe
aburdzy <80163377+aburdzy@users.noreply.github.com>
2021-05-04 18:54:41 +0200

    Dodanie historii rezerwacji na stronie profilu

--- webpage/src/components/Profile.js
+++ webpage/src/components/Profile.js
@@ -11,19 +11,33 @@ import * as api from '../api';
 
 function Profile() {
     let [person, setPerson] = useState([]);
+    let [bookingHistory, setBookingHistory] = useState([]);
+
     let [loading, setLoading] = useState(true);
     let loadingInitTime = Date.now();
     let { role } = useContext(UserContext).user;
 
     useEffect(() => {
-        api.getUserProfile()
-            .then((results) => {
-                setPerson(results);
+        let userProfilePromise = api.getUserProfile()
+            .then(setPerson);
+
+        let bookingsPromise = Promise.resolve();
+        
+        if (role === 'client') {
+            bookingsPromise = api.getUserBookings()
+                .then((results) => {
+                    setBookingHistory(results);
+                    console.log(results);
+                });
+        }
+
+        Promise.all([userProfilePromise, bookingsPromise])
+            .then(() => {
                 setTimeout(() => {
                     setLoading(false);
-                }, Math.max(0, 1000 - (Date.now() - loadingInitTime)));
+                }, Math.max(0, 500 - (Date.now() - loadingInitTime)));
             })
-            .catch(api.errorToString);
+            .catch(api.toastifyError);
     }, []);
 
     return (
@@ -32,7 +46,6 @@ function Profile() {
             <div className="main">
                 <div className="tile half">
                     <h2>Profil</h2>
-                    {console.log(person)}
                     <Person
                         firstName={person.firstName}
                         lastName={person.lastName}
