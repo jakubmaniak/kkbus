83374891f646154d3897ec55b9f0a202e7307bb7
aburdzy <80163377+aburdzy@users.noreply.github.com>
2021-05-04 14:14:09 +0200

    Zmienienie nazw klas w komponencie Person

--- webpage/src/components/Person.js
+++ webpage/src/components/Person.js
@@ -3,27 +3,27 @@ import React from 'react';
 function Person(props) {
     return (
         <>
-            <div className="client-data">
+            <div className="person-data">
                 <span>Imię</span>
                 <span>{props.firstName}</span>
             </div>
-            <div className="client-data">
+            <div className="person-data">
                 <span>Nazwisko</span>
                 <span>{props.lastName}</span>
             </div>
-            <div className="client-data">
+            <div className="person-data">
                 <span>Login</span>
                 <span>{props.login}</span>
             </div>
-            <div className="client-data">
+            <div className="person-data">
                 <span>Adres email</span>
                 <span>{props.email}</span>
             </div>
-            <div className="client-data">
+            <div className="person-data">
                 <span>Data urodzenia</span>
                 <span>{props.birthday}</span>
             </div>
-            <div className="client-data">
+            <div className="person-data">
                 <span>Numer telefonu</span>
                 <span>{props.phoneNumber}</span>
             </div>
