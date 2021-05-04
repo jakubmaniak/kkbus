31b4283b88f081e9545b65b3572b5b038b77b6b2
aburdzy <80163377+aburdzy@users.noreply.github.com>
2021-05-04 16:40:09 +0200

    Dodanie loadera do strony z profilem

--- webpage/src/components/Profile.js
+++ webpage/src/components/Profile.js
@@ -1,22 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import '../styles/Profile.css';
 
 import Person from './Person';
+import { ModalLoader } from './Loader';
+
+import UserContext from '../contexts/User';
+
+import * as api from '../api';
+
 
 function Profile() {
     let [person, setPerson] = useState([]);
+    let [loading, setLoading] = useState(true);
+    let loadingInitTime = Date.now();
+    let { role } = useContext(UserContext).user;
 
     useEffect(() => {
-
+        api.getUserProfile()
+            .then((results) => {
+                setPerson(results);
+                setTimeout(() => {
+                    setLoading(false);
+                }, Math.max(0, 1000 - (Date.now() - loadingInitTime)));
+            })
+            .catch(api.errorToString);
     }, []);
 
     return (
         <div className="profile-page page">
+            <ModalLoader loading={loading} />
             <div className="main">
-                <div className=" half tile">
+                <div className="tile half">
                     <h2>Profil</h2>
-                    <Person />
+                    {console.log(person)}
+                    <Person
+                        firstName={person.firstName}
+                        lastName={person.lastName}
+                        login={person.login}
+                        email={person.email}
+                        birthday={person.birthDate}
+                        phoneNumber={person.phoneNumber}
+                        client={role === 'client' ? true : false}
+                    />
                 </div>
-                <div className=" half tile">
+                <div className="tile half">
                     <h2>Historia rezerwacji</h2>
                 </div>
             </div>
