c8a40b9468d24b0a7a08561aaad0da14951f4cdf
Jakub Maniak <punisher4ce@gmail.com>
2021-05-06 11:52:11 +0200

    Usunięcie zbędnych importów

--- webpage/src/components/Header.js
+++ webpage/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import '../styles/Header.css';
 
