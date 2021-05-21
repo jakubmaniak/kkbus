3b218ec76ca88e812181c66c75441c463664d1d4
aburdzy <80163377+aburdzy@users.noreply.github.com>
2021-05-21 18:56:43 +0200

    Dodanie brakującego tagu

--- webpage/src/components/bookingClient/BookingPage.js
+++ webpage/src/components/bookingClient/BookingPage.js
@@ -31,54 +31,58 @@ function BookingPage() {
                 <div className="tile">
                     <h2>Aktualne rezerwacje</h2>
                     <table className="currentBookings">
-                        <tr>
-                            <th>Data</th>
-                            <th>Godzina</th>
-                            <th>Kierunek</th>
-                            <th>Normalne</th>
-                            <th>Ulgowe</th>
-                            <th>Dzieci do lat 5</th>
-                            <th></th>
-                        </tr>
-                        {futureBookings.map((booking, i) => {
-                            return (
-                                <BookingItem 
-                                    key={i}
-                                    date={new Date(booking.date).toLocaleDateString()}
-                                    hour={booking.hour}
-                                    route={booking.firstStop + ' - ' + booking.lastStop}
-                                    normalTickets={booking.normalTickets}
-                                    reducedTickets={booking.reducedTickets}
-                                    childTickets={booking.childTickets}
-                                />
-                            );
-                        })}
+                        <tbody>
+                            <tr>
+                                <th>Data</th>
+                                <th>Godzina</th>
+                                <th>Kierunek</th>
+                                <th>Normalne</th>
+                                <th>Ulgowe</th>
+                                <th>Dzieci do lat 5</th>
+                                <th></th>
+                            </tr>
+                            {futureBookings.map((booking, i) => {
+                                return (
+                                    <BookingItem 
+                                        key={i}
+                                        date={new Date(booking.date).toLocaleDateString()}
+                                        hour={booking.hour}
+                                        route={booking.firstStop + ' - ' + booking.lastStop}
+                                        normalTickets={booking.normalTickets}
+                                        reducedTickets={booking.reducedTickets}
+                                        childTickets={booking.childTickets}
+                                    />
+                                );
+                            })}
+                        </tbody>
                     </table>
                 </div>
                 <div className="tile">
                     <h2>Historia</h2>
                     <table className="pastBookings">
-                        <tr>
-                            <th>Data</th>
-                            <th>Godzina</th>
-                            <th>Kierunek</th>
-                            <th>Normalne</th>
-                            <th>Ulgowe</th>
-                            <th>Dzieci do lat 5</th>
-                        </tr>
-                        {pastBookings.map((booking, i) => {
-                        return (
-                            <BookingItem 
-                                key={i}
-                                date={new Date(booking.date).toLocaleDateString()}
-                                hour={booking.hour}
-                                route={booking.firstStop + ' - ' + booking.lastStop}
-                                normalTickets={booking.normalTickets}
-                                reducedTickets={booking.reducedTickets}
-                                childTickets={booking.childTickets}
-                            />
-                        );
-                    })}
+                        <tbody>
+                            <tr>
+                                <th>Data</th>
+                                <th>Godzina</th>
+                                <th>Kierunek</th>
+                                <th>Normalne</th>
+                                <th>Ulgowe</th>
+                                <th>Dzieci do lat 5</th>
+                            </tr>
+                            {pastBookings.map((booking, i) => {
+                                return (
+                                    <BookingItem 
+                                        key={i}
+                                        date={new Date(booking.date).toLocaleDateString()}
+                                        hour={booking.hour}
+                                        route={booking.firstStop + ' - ' + booking.lastStop}
+                                        normalTickets={booking.normalTickets}
+                                        reducedTickets={booking.reducedTickets}
+                                        childTickets={booking.childTickets}
+                                    />
+                                );
+                            })}
+                        </tbody>
                     </table>
                 </div>
             </div>
