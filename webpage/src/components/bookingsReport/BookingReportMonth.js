c38563ef8c82111468f57a42db90274b36bf744e
aburdzy <80163377+aburdzy@users.noreply.github.com>
2021-05-26 15:53:47 +0200

    Dodanie wykresów na stronie raporty z rezerwacji

--- webpage/src/components/bookingsReport/BookingReportMonth.js
+++ webpage/src/components/bookingsReport/BookingReportMonth.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/BookingReport.css';
+import Dropdown from '../dropdowns/Dropdown';
+import BookingPieChart from './BookingPieChart';
 
 function BookingReportMonth() {
+    let [data, setData] = useState([]);
+
+    useEffect(() => {
+        setData([
+            {
+                title: 'zrealizowane',
+                value: 22,
+                color: '#47BE61'
+            },
+            {
+                title: 'niezrealizowane',
+                value: 12,
+                color: '#C73535'
+            }
+        ])
+    }, []);
+
     return (
         <div className="tile booking-month">
             <h2>Raport miesięczny z rezerwacji</h2>
+            <Dropdown />
+            <div className="pie-charts-container">
+                <BookingPieChart 
+                        data={data}
+                        label={(props) => { return props.dataEntry.value; }}
+                />
+                <BookingPieChart 
+                    data={data}
+                    label={({ dataEntry }) => { return Math.round(dataEntry.percentage) + '%'; }}
+                />
+            </div>
             <div className="button-container">
                 <button>Drukuj</button>
             </div>
