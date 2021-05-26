b5eed0b0f0c08ef32a89b609f0d70327f7811e49
aburdzy <80163377+aburdzy@users.noreply.github.com>
2021-05-26 16:50:55 +0200

    Dodanie możliwości wyboru miesiąca i roku dla raportu z rezerwacji

--- webpage/src/components/bookingsReport/BookingReportPage.js
+++ webpage/src/components/bookingsReport/BookingReportPage.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BookingReportMonth from './BookingReportMonth';
 import BookingReportYear from './BookingReportYear';
+import Dropdown from '../dropdowns/Dropdown';
 
 function BookingReportPage() {
+    let date = new Date();
+
+    let months = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień'];
+    let [selectedMonth, setSelectedMonth] = useState(months[date.getMonth()]);
+
+    let years = [];
+    years[0] = date.getFullYear();
+    for(let i = 1; i < 10; i++) {
+        years[i] = years[0] + i;
+    }
+    let [selectedYear, setSelectedYear] = useState(date.getFullYear());
+
+    useEffect(() => {
+        console.log(selectedMonth, selectedYear);
+    }, [selectedYear, selectedMonth]);
+
     return (
         <div className="booking-report page">
             <div className="main">
-                <BookingReportMonth />
-                <BookingReportYear />
+                <div className="tile">
+                    <div className="row-filter-container">
+                        <div className="filter-container">
+                            <span>Miesiąc:</span>
+                            <Dropdown
+                                placeholder="Wybierz miesiąc" 
+                                items={months}
+                                selectedIndex={months.indexOf(selectedMonth)}
+                                handleChange={setSelectedMonth}
+                            />
+                        </div>
+                        <div className="filter-container">
+                            <span>Rok:</span>
+                            <Dropdown
+                                placeholder="Wybierz rok" 
+                                items={years}
+                                selectedIndex={years.indexOf(selectedYear)}
+                                handleChange={setSelectedYear}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <BookingReportMonth 
+                    selectedMonth={selectedMonth}
+                    selectedYear={selectedYear}
+                />
+                <BookingReportYear 
+                    selectedMonth={selectedMonth}
+                    selectedYear={selectedYear}
+                />
             </div>
         </div>
     );
