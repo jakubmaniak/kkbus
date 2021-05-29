addfbbbd8ac1e07d38c8d26cd4fc24774c4f02af
aburdzy <80163377+aburdzy@users.noreply.github.com>
2021-05-29 17:48:38 +0200

    Dodanie komponentów dla raportu rocznego z kursów

---
+++ webpage/src/components/routeReport/routeYearReport/RouteYearReport.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RouteYearReport = React.forwardRef((props, ref) => {
+    return (
+        <div className="tile" ref={ref}>
+            <h2>Raport roczny</h2>
+        </div>
+    );
+})
+
+export default RouteYearReport;
