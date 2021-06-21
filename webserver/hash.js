f7dd69ca030b546c316b4f5e75e19a31128edf4c
Jakub Maniak <jakubmaniak99@gmail.com>
2021-06-21 13:21:21 +0200

    Dodanie narzędzia do szyfrowania niezaszyfrowanych haseł w bazie danych

---
+++ webserver/hash.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcrypt');
+const env = require('./helpers/env');
+const userController = require('./controllers/user');
+
+
+if (process.argv.length < 3) {
+    throw new Error('Not enough arguments');
+}
+
+if (process.argv[2] == '*') {
+    updateAllUsers().then(() => process.exit(0));
+}
+else {
+    updateManyUsers().then(() => process.exit(0));
+}
+
+async function updateUser(user) {
+    if (!user.password.startsWith('$b2$')) {
+        let hash = await bcrypt.hash(user.password, env.passwordSalt);
+        await userController.updateUserPassword(user.id, hash);
+    }
+}
+
+async function updateAllUsers() {
+    let users = await userController.findAllUsers();
+    let promises = users.map((user) => updateUser(user));
+
+    return Promise.all(promises);
+}
+
+async function updateManyUsers() {
+    let promises = [];
+
+    for (let i = 2; i < process.argv.length; i++) {
+        let promise = userController.findUserByLogin(process.argv[i])
+            .then(updateUser)
+            .catch((err) => {
+                if (err.message === 'not_found') {
+                    console.log('User ' + process.argv[i] + ' not found');
+                }
+                else {
+                    console.log('Unknown error');
+                }
+            });
+        promises.push(promise);
+    }
+
+    return Promise.all(promises);
+}
