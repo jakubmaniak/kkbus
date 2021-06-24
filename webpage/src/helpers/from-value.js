c8134c5a4c030784f83bd7f72f8d80c5606d081a
Jakub Maniak <jakubmaniak99@gmail.com>
2021-06-24 17:00:26 +0200

    Usunięcie zbędnych funkcji

--- webpage/src/helpers/from-value.js
+++ webpage/src/helpers/from-value.js
@@ -3,31 +3,3 @@ export function fromValue(setter) {
         setter(ev.target.value);
     };
 }
-
-export function intFromValue(setter) {
-    return (ev) => {
-        let int = parseInt(ev.target.value);
-
-        if (isNaN(int)) {
-            throw new TypeError('Entered value cannot be parsed to integer');
-            //console.error('Entered value cannot be parsed to integer');
-            //return;
-        }
-
-        setter(int);
-    };
-}
-
-export function floatFromValue(setter) {
-    return (ev) => {
-        let float = parseFloat(ev.target.value);
-
-        if (isNaN(float)) {
-            throw new TypeError('Entered value cannot be parsed to float');
-            //console.error('Entered value cannot be parsed to float');
-            //return;
-        }
-
-        setter(float);
-    };
-}
